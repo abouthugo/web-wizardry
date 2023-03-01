357c2b229d2c83739d3a87a0ee9f77076fe5db29
abouthugo <hugo@perdomo.dev>
2023-03-01 12:49:03 -0500

    closes #5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+"use client";
 import "./output.css";
 import { Inter } from "@next/font/google";
 import { NavBar } from "../components/NavBar";
-
+import Div100vh from "react-div-100vh";
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
   children,
@@ -12,14 +13,16 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <div
-          className={`flex min-h-screen flex-col bg-neutral-900 ${inter.className}`}
-        >
-          <header className="mb-10">
-            <NavBar />
-          </header>
-          <main className="h-96 flex-1 px-4">{children}</main>
-        </div>
+        <Div100vh>
+          <div
+            className={`flex min-h-screen flex-col bg-neutral-900 ${inter.className}`}
+          >
+            <header className="mb-10">
+              <NavBar />
+            </header>
+            <main className="h-96 flex-1 px-4">{children}</main>
+          </div>
+        </Div100vh>
       </body>
     </html>
   );
