9f90bb65d45dd39fc525107d14782760663b7a14
abouthugo <hugo@perdomo.dev>
2023-04-25 21:05:26 -0400

    adding description and adjusting initial scaling

--- app/blog/head.tsx
+++ app/blog/head.tsx
@@ -1,6 +1,8 @@
 export default function Head() {
   return (
     <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Here is where thoughts go" />
       <title>Blog</title>
     </>
   );
--- app/the-shop/head.tsx
+++ app/the-shop/head.tsx
@@ -1,6 +1,8 @@
 export default function Head() {
   return (
     <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Free until Sunday" />
       <title>The Shop</title>
     </>
   );
