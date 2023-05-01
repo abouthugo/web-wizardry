d7faf3c5259a0f2b9ce252f72477560315e1acd1
abouthugo <hugo@perdomo.dev>
2023-04-30 20:29:00 -0400

    meta tags for preview image, description, and title when sharing the link to homepage

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,32 @@
 export default function Head() {
   return (
     <>
-      <title>Home</title>
+      <title>Welcome to Hugo&apos;s site</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Home of the juice." />
+      <meta
+        name="description"
+        content="Home of the juice. React & Typescript wizard."
+      />
+      <meta content="Hugo's site" property="og:title" />
+      <meta content="Hugo's site" property="twitter:title" />
+      <meta
+        content="Home of the juice. React & Typescript wizard."
+        property="og:description"
+      />
+      <meta
+        content="Home of the juice. React & Typescript wizard."
+        property="twitter:description"
+      />
+      <meta
+        content="https://storage.googleapis.com/wizard-cdn-core/web-preview.jpg"
+        property="og:image"
+      />
+      <meta
+        content="https://storage.googleapis.com/wizard-cdn-core/web-preview.jpg"
+        property="twitter:image"
+      />
+      <meta property="og:type" content="website" />
+      <meta content="summary_large_image" name="twitter:card" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
