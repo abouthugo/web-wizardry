50e88e21419b897f2a92a62b16648b58082c3265
abouthugo <hugo@perdomo.dev>
2023-06-08 23:13:46 -0400

    closes #12 migrating to remark for parsing MD files

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,31 +1,18 @@
-import fs from "fs/promises";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-import showdown from "showdown";
 import { Title } from "../../../components/Typography";
+import { getPostData } from "../../../lib/posts";
 import styles from "./page.module.css";
 
-async function getBlogPost(blogId: string) {
-  const converter = new showdown.Converter({ simpleLineBreaks: true });
-  const posts: PostsRepo = require("../../../content/blogs.json");
-  const { blogs } = posts;
-  const blogPost = blogs.find((t) => t.id === blogId);
-  if (!blogPost) return null;
-
-  const mdPath = `content/${blogPost.content}`;
-  const blogText = await fs.readFile(mdPath, "utf-8");
-  return { ...blogPost, text: converter.makeHtml(blogText) };
-}
-
 export default async function BlogPost({ params }: any) {
-  const blog = await getBlogPost(params.id);
+  const blog = await getPostData(params.id);
   if (!blog) notFound();
   return (
     <div className="max-w-3xl mx-auto">
-      <Title>{blog.title}</Title>
+      <Title>{blog.data.title}</Title>
       <Image
-        src={blog.image.src}
-        alt={blog.image.alt}
+        src={blog.data.image}
+        alt={blog.data.alt}
         className="rounded-md mb-10"
         width={700}
         height={160}
@@ -33,7 +20,7 @@ export default async function BlogPost({ params }: any) {
       />
       <div
         className={styles.wrapper}
-        dangerouslySetInnerHTML={{ __html: blog.text }}
+        dangerouslySetInnerHTML={{ __html: blog.contentHTML }}
       ></div>
     </div>
   );
@@ -48,8 +35,4 @@ type BlogPost = {
   };
   date: string;
   content: string;
-};
-
-type PostsRepo = {
-  blogs: BlogPost[];
 };
--- components/BlogPostPreview.tsx
+++ components/BlogPostPreview.tsx
@@ -1,31 +1,31 @@
 import Link from "next/link";
 import Image from "next/image";
 
-// TODO: make this into a reusable component
-export default function BlogPostPreview() {
+export default function BlogPreview({ blog }: { blog: BlogData }) {
   return (
     <article className="max-w-3xl mx-auto">
-      <Link href="/blog/1">
+      <Link href={`/blog/${blog.id}`}>
         <div className="flex  justify-between outline-white outline-1 hover:bg-neutral-800 hover:cursor-pointer rounded-md p-4">
           <div>
-            <p className="text-sm text-neutral-500">Jan 24, 2023</p>
-            <h1 className="text-2xl font-bold">Withering Garden</h1>
-            <p className="text-neutral-400 text-md mb-2">
-              I find that as I&apos;m getting older I&apos;m able to easily find
-              more things to write about.
-            </p>
-            <span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-              Poems
-            </span>
+            <p className="text-sm text-neutral-500">{blog.date}</p>
+            <h1 className="text-2xl font-bold">{blog.title}</h1>
+            <p className="text-neutral-400 text-md mb-2">{blog.preview}</p>
+            {blog.tags.split(", ").map((t) => (
+              <span
+                key={t}
+                className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
+              >
+                {t}
+              </span>
+            ))}
           </div>
           <div>
             <Image
-              src="/withering-garden.png"
-              alt="Withering Garden"
+              src={blog.image}
+              alt={blog.alt}
               className="rounded-md"
               width={100}
               height={40}
-              priority
             />
           </div>
         </div>
@@ -33,3 +33,13 @@ export default function BlogPostPreview() {
     </article>
   );
 }
+
+export type BlogData = {
+  id: number;
+  title: string;
+  tags: string;
+  date: string;
+  preview: string;
+  image: string;
+  alt: string;
+};
