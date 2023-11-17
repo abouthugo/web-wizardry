632783517741b371dd58554c0fc2be1dc9c2484d
abouthugo <hugo@perdomo.dev>
2023-11-17 11:24:45 -0500

    adds meta title and preview for liz page

---
+++ app/liz/head.tsx
@@ -0,0 +1,24 @@
+export default function Head() {
+  return (
+    <>
+      <title>Hi Liz</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="A page for my love" />
+      <meta content="Liz's page" property="og:title" />
+      <meta content="Liz's page" property="twitter:title" />
+      <meta content="A page for my love" property="og:description" />
+      <meta content="A page for my love" property="twitter:description" />
+      <meta
+        content="https://storage.googleapis.com/wizard-cdn-core/web-prev-liz.jpg"
+        property="og:image"
+      />
+      <meta
+        content="https://storage.googleapis.com/wizard-cdn-core/web-prev-liz.jpg"
+        property="twitter:image"
+      />
+      <meta property="og:type" content="website" />
+      <meta content="summary_large_image" name="twitter:card" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
