2f325d9e82904e6ac7eb2189424582b5c82b584d
Hugo Perdomo <hugo@perdomo.dev>
2024-04-03 08:10:51 -0400

    using terniary to conditionally render

--- components/BigCountdown.tsx
+++ components/BigCountdown.tsx
@@ -5,15 +5,15 @@ const Box = (props: { children: React.ReactNode }) => <div className="text-cente
 
 export default function BigCountdown() {
   const bigDate = DateTime.fromISO("2024-11-09");
-  const { weeks, days, hours, minutes } = bigDate.diffNow(["weeks", "days", "hours", "minutes"]).toObject();
+  const { weeks, days, hours, minutes } = bigDate.diffNow(["weeks", "days", "hours", "minutes", "seconds"]).toObject();
   return (
     <div className="text-white container mx-auto text-center mb-12">
       <h1 className="text-2xl">Wedding Day</h1>
       <div className="flex gap-2 justify-center items-center">
-        {weeks && weeks >= 1 && <Box>{weeks} Weeks</Box>}
-        {days && days >= 1 && <Box>{days} Days</Box>}
-        {hours && hours >= 1 && <Box>{hours && Math.floor(hours)} Hours</Box>}
-        {minutes && minutes >= 1 && <Box>{minutes && Math.floor(minutes)} Minutes</Box>}
+        {weeks && weeks >= 1 ? <Box>{weeks} Weeks</Box> : null}
+        {days && days >= 1 ? <Box>{days} Days</Box> : null}
+        {hours && hours >= 1 ? <Box>{hours} Hours</Box> : null}
+        {minutes && minutes >= 1 ? <Box>{minutes} Minutes</Box> : null}
       </div>
     </div>
   );
