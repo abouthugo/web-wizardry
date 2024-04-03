50cdd6d2c651c459371a95d065bdcd4b7be94b3b
Hugo Perdomo <hugo@perdomo.dev>
2024-04-02 23:13:41 -0400

    fixing errors for countdown

--- components/BigCountdown.tsx
+++ components/BigCountdown.tsx
@@ -1,36 +1,20 @@
 "use client";
-import { DateTime } from "luxon"
+import { DateTime } from "luxon";
 
-const Box = (props: { children: React.ReactNode }) => (
-  <div className="text-center p-4 text-md">
-    {props.children}
-  </div>
-)
+const Box = (props: { children: React.ReactNode }) => <div className="text-center p-4 text-md">{props.children}</div>;
 
 export default function BigCountdown() {
   const bigDate = DateTime.fromISO("2024-11-09");
-  const { months, weeks, days, hours, minutes } = bigDate.diffNow(["months", "weeks", "days", "hours", "minutes"]).toObject();
+  const { weeks, days, hours, minutes } = bigDate.diffNow(["weeks", "days", "hours", "minutes"]).toObject();
   return (
     <div className="text-white container mx-auto text-center mb-12">
       <h1 className="text-2xl">Wedding Day</h1>
-      <div className="flex gap-2 justify-center">
-        <Box>
-          {months} Months
-        </Box>
-        {weeks >= 1 && <Box>
-          {weeks} Weeks
-        </Box>
-        }
-        <Box>
-          {days} Days
-        </Box>
-        <Box>
-          {hours && Math.floor(hours)} Hours
-        </Box>
-        <Box>
-          {minutes && Math.floor(minutes)} Minutes
-        </Box>
+      <div className="flex gap-2 justify-center items-center">
+        {weeks && weeks >= 1 && <Box>{weeks} Weeks</Box>}
+        {days && days >= 1 && <Box>{days} Days</Box>}
+        {hours && hours >= 1 && <Box>{hours && Math.floor(hours)} Hours</Box>}
+        {minutes && minutes >= 1 && <Box>{minutes && Math.floor(minutes)} Minutes</Box>}
       </div>
     </div>
-  )
+  );
 }
