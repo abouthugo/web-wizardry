66a798df5de43833e9359732c3d56d2361560d45
Hugo Perdomo <hugo@perdomo.dev>
2024-05-18 12:19:30 -0400

    disabling hearts component for now

--- app/liz/page.tsx
+++ app/liz/page.tsx
@@ -3,8 +3,8 @@ import ClientMessage from "@components/ClientMessage";
 import Counter from "./Counter";
 import ImageSet from "@components/ImageSet";
 import type { Metadata } from "next";
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "H&E",
@@ -25,37 +25,37 @@ export const metadata: Metadata = {
   },
 };
 
-const HeartsComponent = () => {
-  const heartWidth = 90;
-  const heartHeight = 100;
-  return (
-    <div className="flex justify-center items-center mx-auto mt-12 mb-20">
-      <div className="relative outline-none group">
-        <Image
-          className="relative z-10"
-          src="/images/blended-heart-alt.png"
-          width={heartWidth}
-          height={heartHeight}
-          alt="blue heart"
-        />
-        <div className="absolute left-0 top-0 h-full w-full duration-1500 ">
-          {Array(8)
-            .fill(0)
-            .map((_, i) => (
-              <Image
-                className={styles["phantom-card"]}
-                key={`${i}-phantom-image`}
-                src="/images/blended-heart.png"
-                width={heartWidth}
-                height={heartHeight}
-                alt="blue heart"
-              />
-            ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+// const HeartsComponent = () => {
+//   const heartWidth = 90;
+//   const heartHeight = 100;
+//   return (
+//     <div className="flex justify-center items-center mx-auto mt-12 mb-20">
+//       <div className="relative outline-none group">
+//         <Image
+//           className="relative z-10"
+//           src="/images/blended-heart-alt.png"
+//           width={heartWidth}
+//           height={heartHeight}
+//           alt="blue heart"
+//         />
+//         <div className="absolute left-0 top-0 h-full w-full duration-1500 ">
+//           {Array(8)
+//             .fill(0)
+//             .map((_, i) => (
+//               <Image
+//                 className={styles["phantom-card"]}
+//                 key={`${i}-phantom-image`}
+//                 src="/images/blended-heart.png"
+//                 width={heartWidth}
+//                 height={heartHeight}
+//                 alt="blue heart"
+//               />
+//             ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 const months = [
   {
     title: "July",
