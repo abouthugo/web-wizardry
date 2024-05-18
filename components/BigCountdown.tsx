a35822ce1b248befb7c0423b77e77b1f5f94bf6e
Hugo Perdomo <hugo@perdomo.dev>
2024-05-18 12:08:48 -0400

    polishing

--- components/BigCountdown.tsx
+++ components/BigCountdown.tsx
@@ -1,48 +1,31 @@
 "use client";
+import styles from "./BigCountdown.module.css";
 import classNames from "classnames";
 import { DateTime, type DurationUnit } from "luxon";
-import type { PropsWithChildren, FunctionComponent } from "react";
 
 type DateObject = {
-  weeks: number;
   days: number;
   hours: number;
   minutes: number;
   seconds: number;
 };
 
-type BoxProps = PropsWithChildren<{
-  className?: string;
-}>;
-
-const Box: FunctionComponent<BoxProps> = ({ children, className }) => {
-  return (
-    <div className={classNames("text-center p-4 text-md", className)}>
-      {children}
-    </div>
-  );
-};
-
 export default function BigCountdown() {
-  const units: DurationUnit[] = [
-    "weeks",
-    "days",
-    "hours",
-    "minutes",
-    "seconds",
-  ];
+  const units: DurationUnit[] = ["days", "hours", "minutes", "seconds"];
   const wd = DateTime.fromISO("2024-11-09");
-  const { weeks, days, hours, minutes } = wd
-    .diffNow(units)
-    .toObject() as DateObject;
+  const { days } = wd.diffNow(units).toObject() as DateObject;
   return (
-    <div className="text-white container mx-auto text-center mb-12">
-      <h1 className="text-2xl">Wedding Day</h1>
-      <div className="flex gap-2 justify-center items-center">
-        {weeks >= 1 ? <Box>{weeks} Weeks</Box> : null}
-        {days >= 1 ? <Box>{days} Days</Box> : null}
-        {hours >= 1 ? <Box>{hours} Hours</Box> : null}
-        {minutes >= 1 ? <Box>{minutes} Minutes</Box> : null}
+    <div
+      className={classNames(
+        "flex justify-center items-center my-12 h-[100vmax] w-full rounded-xl",
+        styles.container,
+      )}
+    >
+      <div className="text-xl text-neutral-300 text-center">
+        {days >= 1 ? (
+          <div className="text-3xl font-bold text-white">{days} days</div>
+        ) : null}{" "}
+        till wedding day
       </div>
     </div>
   );
