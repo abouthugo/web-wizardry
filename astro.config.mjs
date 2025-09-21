3aa01a8c723e3e34e5028924691490af68664e28
Hugo Perdomo <hugo@perdomo.dev>
2025-09-21 18:35:02 -0400

    add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.hugoperdomo.com"
+  site: "https://www.hugoperdomo.com",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
